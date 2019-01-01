10342598518e743ce68f39e2f8d892f0630e4124
Ahmad Alsaadi <saadias@Ahmads-MacBook-Pro.local>
2019-01-01 10:52:32 +0300

    adding book files

---
+++ book code.py
@@ -0,0 +1,13 @@
+Python 3.6.1 |Anaconda custom (64-bit)| (default, May 11 2017, 13:04:09) 
+[GCC 4.2.1 Compatible Apple LLVM 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+
+>>> set_1={1,2,'a','b'}
+>>> set_2={1, 'd', 2, 'c'}
+>>> set_3={'a','b'}
+
+>>> set_3.issubset(set_1)
+True
+>>> set_2.issubset(set_1)
+False
+>>>
